a3b6ca8144caafb4e8e9c75159de1305ec965eba
James Smith <js5@sanger.ac.uk>
2019-07-08 14:54:48 +0100

    pusing changes to pub script

--- pubs.js
+++ pubs.js
@@ -1,4 +1,16 @@
+function show_pubs(x) {
+  console.log( x );
+  console.log( $(x) );
+  $(x).load('/component/references?pars='+encodeURIComponent($(x).data('ids')) );
+}
+/*
 $('.ajax_publications').each(function() {
+  if( $(body).has_class('ajax_pub_processed') ) {
+    return;
+  }
   console.log( '/component/references?pars='+encodeURIComponent($(this).data('ids')) );
-  $(this).load('/component/references?pars='+encodeURIComponent($(this).data('ids')) );
+  show_pubs(this);
+  $(body).add_class('ajax_pub_processed');  
 });
+
+*/
