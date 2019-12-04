753c27bd6266e828245dd43d9808abd6767eb02d
James Smith <js5@sanger.ac.uk>
2019-12-04 10:45:20 +0000

    fixed markup

--- base-theme-class.php
+++ base-theme-class.php
@@ -52,9 +52,9 @@
  */
 
 const EXTRA_SETUP = [
-  'date_picker'      => [ 'return_value' => 'Y-m-d'       ], // Return values in "mysql datetime" format
-  'date_time_picker' => [ 'return_value' => 'Y-m-d H:i:s' ], //  - or relevant part of....
-  'time_picker'      => [ 'return_value' => 'H:i:s'       ], //
+  'date_picker'      => [ 'return_format' => 'Y-m-d'       ], // Return values in "mysql datetime" format
+  'date_time_picker' => [ 'return_format' => 'Y-m-d H:i:s' ], //  - or relevant part of....
+  'time_picker'      => [ 'return_format' => 'H:i:s'       ], //
   'image'            => [ 'save_format' => 'object', 'library' => 'all', 'preview_size' => 'large' ],
   'medium_editor'    => [
     'standard_buttons' => [ 'bold', 'italic', 'subscript', 'superscript', 'removeFormat' ],
