4e8bfa48b48b36e4df84f26986c7f7b162bb58fe
James Smith <js5@sanger.ac.uk>
2020-06-02 11:49:41 +0100

    update version no to 0.1.5

--- base-theme-class.php
+++ base-theme-class.php
@@ -42,7 +42,7 @@
  * Plugin Name: Website Base Theme Class
  * Plugin URI:  https://jamessmith.me.uk/base-theme-class/
  * Description: Support functions to apply simple templates to acf pro data structures!
- * Version:     0.1.4
+ * Version:     0.1.5
  * Author:      James Smith
  * Author URI:  https://jamessmith.me.uk
  * Text Domain: base-theme-class-locale
