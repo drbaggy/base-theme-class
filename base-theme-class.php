48646076f2e9241896ba177e72e94dab2776f80f
www-core for web team <www-core@sanger.ac.uk>
2020-12-01 11:22:32 +0000

    V0.3.1

--- base-theme-class.php
+++ base-theme-class.php
@@ -45,7 +45,7 @@
                  * apply simple templates to acf pro data structures!
                  * to fix annoying defaults in wordpress
                  * to handle sanger publications
- * Version:     0.3.0
+ * Version:     0.3.1
  * Author:      James Smith
  * Author URI:  https://jamessmith.me.uk
  * Text Domain: base-theme-class-locale
